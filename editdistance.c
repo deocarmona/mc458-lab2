a3dd404dd3cebcb772ad6188f55a4ec5ff63a600
Victor Carmona <victordeocarmona@gmail.com>
2013-10-26 11:11:56 -0200

    commit de teste1

---
+++ editdistance.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+
+//apenas a funcao do programa (pros outros, apenas essa parte é diferente, o resto é igual)
+
+
+
+int main(int argc, char *argv[]){
+
+	printf("Hello World\n");
+	
+	return 0;
+}
