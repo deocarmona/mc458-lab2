e254247e164a3d0c1afc52ba73e591b32f5b5183
Victor Carmona <victordeocarmona@gmail.com>
2013-10-26 11:53:02 -0200

    lendo do arquivo do jeito proposto, falta implementar a programacao dinamica

--- editdistance.c
+++ editdistance.c
@@ -11,7 +11,57 @@
 
 int main(int argc, char *argv[]){
 
-	printf("Hello World\n");
+	FILE *fp;	
+	FILE *saida;
+	//abre arquivo pra leitura;
+	fp = fopen(argv[1], "r");
+	if (!fp) printf ("Erro na abertura do arquivo.");
 	
+	saida = fopen("Distances.txt", "w");
+
+ int tam;
+ int i;
+ char aux;
+
+ 	//fprintf(saida,"tempos:");
+	
+		fscanf(fp,"%d\n", &tam);
+		char *vetorx;
+		char *vetory;
+   vetorx = (char*) malloc (tam*sizeof(char));
+		vetory = (char*) malloc (tam*sizeof(char));
+
+
+  for (i=0;i<tam;i++){
+  	fscanf(fp,"%c\n", &aux);
+  	//vai colocando os valores no vetor
+		vetorx[i] = aux;
+		fscanf(fp,"%c\n", &aux);
+  	//vai colocando os valores no vetor
+		vetory[i] = aux;
+		fscanf(fp,"\n");	
+			
+	}
+	
+	 for (i=0;i<tam;i++){
+			fprintf(saida,"%c ",vetorx[i]);
+			printf("%c ",vetorx[i]);
+	}
+	//fprintf(saida,"\n");
+	for (i=0;i<tam;i++){
+			fprintf(saida,"%c ",vetory[i]);
+			printf("%c ",vetory[i]);
+	}
+
+
+
+	printf("\n");
+
+	free(vetorx);
+	free(vetory);
+
+
+	fclose(fp);
+	fclose(saida);
 	return 0;
 }
