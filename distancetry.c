d57e6091dbd310974fb8abeb0de610fe68f80555
deocarmona <victordeocarmona@gmail.com>
2013-11-09 00:11:41 -0200

    tentativa matriz custo e op

--- distancetry.c
+++ distancetry.c
@@ -1,4 +1,4 @@
-Cc, Cr, Cd, Ci, Ct, Ck
+//Cc, Cr, Cd, Ci][Ct, Ck
 //retorna a matriz de custo e a matriz de operação
 
 		int *T;
@@ -15,48 +15,48 @@ Cc, Cr, Cd, Ci, Ct, Ck
 EDIT-DISTANCE(x, y, m, n){
 	//coloca custo de delete em todas as colunas 0
 	for(i=0;i<m;i++){
-			T[i, 0] = i * Cd;//Custo delete
-			op[i, 0] = "d";
+			T[i][0] = i * Cd;//Custo delete
+			op[i][0] = "d";
 	}
 	//coloca custo de insert em todas as linhas 0
 	for(j=0;j<n;j++){
-			T[0, j ] = j * Ci;//Custo insert
-			op[0, j] = "i";
+			T[0][j] = j * Ci;//Custo insert
+			op[0][j] = "i";
 	}
-	//percorre a matriz de custo 
+			//percorre a matriz de custo 
 	for(i=1;i<m;i++){
 			for (j=1;j<n;j++){
-					T[i, j] = ∞;//como colocar um peso maior que os custos????
+					T[i][j] = 444;//como colocar um peso maior que os custos????
 					if (x[i] == y[j]){//se as letras forem iguais copia
-							T[i, j] = T[i−1, j−1] + Cc;//Custo copy
-							op[i, j] = "c";
+							T[i][j] = T[i-1][j-1] + Cc;//Custo copy
+							op[i][j] = 'c';
 					}		
 					//se o custo de troca for menor
-					if ((x[i] != y[j]) && ((T[i−1, j−1] + Cr) < T[i, j])){//Custo replace
-							T[i, j] = T[i−1, j−1] + Cr;//Custo replace
-							op[i, j] = "r";//REPLACE(by y[j]);
+					if ((x[i] != y[j]) && ((T[i-1][j-1] + Cr) < T[i][j])){//Custo replace
+							T[i][j] = T[i-1][j-1] + Cr;//Custo replace
+							op[i][j] = 'r';//REPLACE(by y[j]);
 					}				
 					//	
-					if (( i ≥ 2) && (j ≥ 2) && (x[i] == y[j−1]) && 
-					(x[i−1] == y[j]) && ((T[i−2, j−2] + Ct) < T[i, j])){//Custo twiddle
-							T[i, j] = T[i−2, j−2] + Ct;//Custo twiddle
-							op[i, j] = "t";
+					if (( i >= 2) && (j >= 2) && (x[i] == y[j-1]) && 
+					(x[i-1] == y[j]) && ((T[i-2][j-2] + Ct) < T[i][j])){//Custo twiddle
+							T[i][j] = T[i-2][j-2] + Ct;//Custo twiddle
+							op[i][j] = 't';
 					}
 					//				
-					if ((T[i−1, j] + cost(delete)) < T[i, j]){
-							T[i, j] = T[i−1, j] + Cd//Custo delete
-							op[i, j] = "d";
+					if ((T[i-1][j] + cost(delete)) < T[i][j]){
+							T[i][j] = T[i-1][j] + Cd//Custo delete
+							op[i][j] = 'd';
 					}
-					if (T[i, j−1] + Ci < T[i, j]){
-							T[i, j] = T[i, j−1] + Ci//Custo insert
-							op[i, j] = "i";//INSERT(y[j]);
+					if (T[i][j-1] + Ci < T[i][j]){
+							T[i][j] = T[i][j-1] + Ci//Custo insert
+							op[i][j] = 'i';//INSERT(y[j]);
 					}
 			}
 	}		
 	for(i=0;i<m-1;i++){
-			if (T[i, n] + Ck < T[m, n]){
-					T[m, n] = T[i, n] + Ck;//Custo
-					op[m, n] = "k";//KILL(i);
+			if (T[i][n] + Ck < T[m][n]){
+					T[m][n] = T[i][n] + Ck;//Custo
+					op[m][n] = 'k';//KILL(i);
 			}
 	}
 	return;// T and op;
@@ -67,57 +67,57 @@ EDIT-DISTANCE(x, y, m, n){
 //imprime a operação que está sendo realizada
 OP-SEQUENCE(op, i, j){
 if (i == 0 && j == 0) 		return;
-if ((op[i, j] == "c") || (op[i, j] == "r")){
+if ((op[i][j] == "c") || (op[i][j] == "r")){
 		aux_i = i − 1;
 		aux_j = j − 1;
-}else if (op[i, j] == "t"){
+}else if (op[i][j] == "t"){
 		aux_i = i − 2;
 		aux_j = j − 2;
-}else if ()op[i, j] == "d"){
+}else if ()op[i][j] == "d"){
 		aux_i = i − 1;
 		aux_j = j;
-}elseif (op[i, j] == "i"){
+}elseif (op[i][j] == "i"){
 		aux_i = i;
 		aux_j = j − 1;
 }else{
-		let op[i, j] == KILL k;
-		aux_i = k;
+		op[i][j] == "k";
+		aux_i = m;
 		aux_j = j;
 }
 	OP-SEQUENCE(op, aux_i , aux_j);
-	if ((op[i, j] == "c") printf("%d %d Copy %c", i, j, vetory[j]);
-	if ((op[i, j] == "r") printf("%d %d Replace %c", i, j, vetory[j]);
-	if ((op[i, j] == "t") printf("%d %d Twiddle %c%c", i, j, vetory[j-1],vetory[j]);
-	if ((op[i, j] == "d") printf("%d %d Delete _", i, j);
-	if ((op[i, j] == "i") printf("%d %d Insert %c", i, j, vetory[j]);
-	if ((op[i, j] == "k") printf("%d %d Kill *", i, j);
+	if ((op[i][j] == "c") printf("%d %d Copy %c", i, j, vetory[j]);
+	if ((op[i][j] == "r") printf("%d %d Replace %c", i, j, vetory[j]);
+	if ((op[i][j] == "t") printf("%d %d Twiddle %c%c", i, j, vetory[j-1],vetory[j]);
+	if ((op[i][j] == "d") printf("%d %d Delete _", i, j);
+	if ((op[i][j] == "i") printf("%d %d Insert %c", i, j, vetory[j]);
+	if ((op[i][j] == "k") printf("%d %d Kill *", i, j);
 }
 
 
 //acha a resposta certa percorrendo a matrix de custo
-//T[i, j] = min 
-/*---- acho que essa parte ele só explica o que é T[i,j] não é codigo---
+//T[i][j] = min 
+//---- acho que essa parte ele só explica o que é T[i,j] não é codigo---
 int Minim(T,i,j,m,n){
-del = T[i − 1, j] + Cd;//Custo delete
-ins = T[i, j − 1] + Ci;//custo insert
+del = T[i − 1][j] + Cd;//Custo delete
+ins = T[i][j − 1] + Ci;//custo insert
 
 	if (x[i]==y[j]){
-			cop = T[i − 1, j − 1] + Cc;//Custo copy
+			cop = T[i − 1][j − 1] + Cc;//Custo copy
 	}
 	if (x[i] != y[j]){
-			rep = T[i − 1, j − 1] + Cr;//Custo replace
+			rep = T[i − 1][j − 1] + Cr;//Custo replace
 		}
 	if ((i>=2) && (j >= 2) && (x[i] == y[j−1]) && (x[i−1] == y[j])){
-			twid = T[i − 2, j − 2] + Ct; //Custo twiddle
+			twid = T[i − 2][j − 2] + Ct; //Custo twiddle
 	}
 	if ((i == m) && (j == n)){
-			aux = T[0,n];	
+			aux = T[0][n];	
 			for (k=1;k<m;k++){
-					if ( T[k,n]<aux){
-							aux = T[k,n];						
+					if ( T[k][n]<aux){
+							aux = T[k][n];			
 						}
 			}
-			kil = T[k, n] + cost(kill(m-k))//Custo kill
+			kil = T[k][n] + cost(kill(m-k))//Custo kill
 	}
 	//retorna o menor custo NAO SEI FAZER MELHOR
 	if ((del<ins)&&(del<cop)&&(del<rep)&&(del<twid)&&(del<kil)) return del;
--- editdistance.c
+++ editdistance.c
@@ -4,13 +4,139 @@
 #include <string.h>
 #include <time.h>
 
+//acha a resposta certa percorrendo a matrix de custo
+//T[i][j] = min 
+//---- acho que essa parte ele só explica o que é T[i,j] não é codigo---
+int minimo_custo(char x[], char y[],int i,int j,int m,int n,int* T[m],int Cc,int Cr,int Cd,int Ci,int Ct,int Ck){
+	int del=4444, ins=4444,cop=4444, rep=4444,twid=4444,kil=4444;
+	char aux;
+	int k;
+del = T[i - 1][j] + Cd;//Custo delete
+ins = T[i][j - 1] + Ci;//custo insert
+
+	if (x[i]==y[j]){
+			cop = T[i - 1][j - 1] + Cc;//Custo copy
+	}
+	if (x[i] != y[j]){
+			rep = T[i - 1][j - 1] + Cr;//Custo replace
+		}
+	if ((i>=2) && (j >= 2) && (x[i] == y[j-1]) && (x[i-1] == y[j])){
+			twid = T[i - 2][j - 2] + Ct; //Custo twiddle
+	}
+	if ((i == m) && (j == n)){
+			aux = T[0][n];		
+			for (k=1;k<m;k++){
+					if ( T[k][n]<aux){
+							aux = T[k][n];			
+						}
+			}
+			kil = T[k][n] + Ck;//Custo kill
+	}
+	//retorna o menor custo NAO SEI FAZER MELHOR
+	if ((cop<=ins)&&(cop<=del)&&(cop<=rep)&&(cop<=twid)&&(cop<=kil)) return cop;
+	if ((ins<=del)&&(ins<=cop)&&(ins<=rep)&&(ins<=twid)&&(ins<=kil)) return ins;
+	if ((rep<=ins)&&(rep<=cop)&&(rep<=del)&&(rep<=twid)&&(rep<=kil)) return rep;
+	if ((del<=ins)&&(del<=cop)&&(del<=rep)&&(del<=twid)&&(del<=kil)) return del;
+	if ((twid<=ins)&&(twid<=cop)&&(twid<=rep)&&(twid<=del)&&(twid<=kil)) return twid;	
+	if ((kil<=ins)&&(kil<=cop)&&(kil<=rep)&&(kil<=twid)&&(kil<=del)) return kil;
+	return 0;
+}
+
+void seq_op(int i, int j, char vetory[],char* op[i],int m,int valor_i_kill){
+	//o ultimo elemento da matriz não tem operador
+	int aux_i, aux_j;
+if (i == 0 && j == 0) 		return;
+if ((op[i][j] == 'c') || (op[i][j] == 'r')){
+		aux_i = i - 1;
+		aux_j = j - 1;
+}else if (op[i][j] == 't'){
+		aux_i = i - 2;
+		aux_j = j - 2;
+}else if (op[i][j] == 'd'){
+		aux_i = i - 1;
+		aux_j = j;
+}else if (op[i][j] == 'i'){
+		aux_i = i;
+		aux_j = j - 1;
+}else{
+		op[i][j] == 'k';
+		aux_i = valor_i_kill;
+		aux_j = j;
+}
+	seq_op(aux_i,aux_j,vetory,op,m,valor_i_kill);
+
+	if (op[i][j] == 'c') printf("%d %d Copy %c\n", i, j, vetory[j-1]);
+	if (op[i][j] == 'r') printf("%d %d Replace %c\n", i, j, vetory[j-1]);
+	if (op[i][j] == 't') printf("%d %d Twiddle %c%c\n", i, j, vetory[j-2],vetory[j-1]);
+	if (op[i][j] == 'd') printf("%d %d Delete _\n", i, j);
+	if (op[i][j] == 'i') printf("%d %d Insert %c\n", i, j, vetory[j-1]);
+	if (op[i][j] == 'k') printf("%d %d Kill *\n", i, j);
+}
+
+
+void edit_distances(char x[], char y[], int m, int n,int Cc,int Cr,int Cd,int Ci,int Ct,int Ck,int* T[m],char* op[m],int* valor_i_kill){
+	int i;
+	int j;
+	//coloca custo de delete em todas as colunas 0
+	for(i=1;i<m+1;i++){
+			T[i][0] = Cd*i ;//Custo delete
+			op[i][0] = 'd';
+	}
+	//coloca custo de insert em todas as linhas 0
+	for(j=1;j<n+1;j++){
+			T[0][j] = Ci*j;//Custo insert
+			op[0][j] = 'i';
+	}
+	//T[0][0]=0;
+	op[0][0]=' ';
+		//percorre a matriz de custo 
+	for(i=1;i<m+1;i++){
+			for (j=1;j<n+1;j++){
+					
+					T[i][j] = minimo_custo(x, y,i,j,m,n,T,Cc,Cr,Cd,Ci,Ct,Ck);
+					
+					if (x[i-1] == y[j-1]){//se as letras forem iguais copia
+							T[i][j] = Cc;//T[i-1][j-1];//Custo copy
+							op[i][j] = 'c';
+					}	
+					//se o custo de troca for menor
+					if ((x[i-1] != y[j-1]) && ((T[i-1][j-1] + Cr) == T[i][j])){//Custo replace
+							T[i][j] = Cr;// +T[i-1][j-1];//Custo replace
+							op[i][j] = 'r';//REPLACE(by y[j]);
+					}				
+					//	
+					if (( i >= 2) && (j >= 2) && (x[i] == y[j-1]) && 
+					(x[i-1] == y[j]) && ((T[i-2][j-2] + Ct) == T[i][j])){//Custo twiddle
+							T[i][j] = Ct;//+T[i-2][j-2];//Custo twiddle
+							op[i][j] = 't';
+					}
+					//				
+					if ((T[i-1][j] + Cd) == T[i][j]){
+							T[i][j] = Cd;//+T[i-1][j] ;//Custo delete
+							op[i][j] = 'd';
+					}
+					if (T[i][j-1] + Ci == T[i][j]){
+							T[i][j] = Ci;//+T[i][j-1];//Custo insert
+							op[i][j] = 'i';//INSERT(y[j]);
+					}
+			}
+	}		
+	for(i=0;i<m+1;i++){
+			if (T[i][n] + Ck < T[m][n]){
+					T[m][n] = Ck;//+T[i][n];//Custo
+					op[m][n] = 'k';//KILL(i);
+					valor_i_kill = i;
+			}
+	}
+	return;// T and op;
+}
 
 //apenas a funcao do programa (pros outros, apenas essa parte é diferente, o resto é igual)
 
-
-
+		 
 int main(int argc, char *argv[]){
 
+	
 	FILE *fp;	
 	FILE *saida;
 	//abre arquivo pra leitura;
@@ -18,59 +144,80 @@ int main(int argc, char *argv[]){
 	if (!fp) printf ("Erro na abertura do arquivo.");
 	
 	saida = fopen("Distances.txt", "w");
-
+	int valor_i_kill;
  int m, n;
- int i;
- int Cc, Cr, Cd, Ci, Ct, Ck;
+ int i,j;
+	int Cc, Cr, Cd, Ci, Ct, Ck;
  char aux;
 
- 	//fprintf(saida,"tempos:");
+		//le do arquivo de entrada o tamanho das palavras
 	
 		fscanf(fp,"%d %d\n", &m, &n);
-		char *vetorx;
-		char *vetory;
-   vetorx = (char*) malloc (m*sizeof(char));
-		vetory = (char*) malloc (n*sizeof(char));
+		//cria os vetores de palavras
+		char *x;
+		char *y;
+   x = (char*) malloc ((m+1)*sizeof(char));
+		y = (char*) malloc ((n+1)*sizeof(char));
 
-
+		//cria as matrizes de custo e de operações
+	 	int **T;
+		char **op;
+   T = malloc ((m+1)*sizeof(int*));
+   for (i=0;i<m+1;i++){
+				T[i] = (int*) malloc ((n+1)*sizeof(int));
+		}
+		op = malloc ((m+1)*sizeof(char*));
+   for (i=0;i<m+1;i++){
+				op[i] = (char*) malloc ((n+1)*sizeof(char));
+		}
+		
+	//le o arquivo colocando nos vetores as palavras correspondentes
   for (i=0;i<m;i++){
   	fscanf(fp,"%c", &aux);
   	//vai colocando os valores no vetor
-		vetorx[i] = aux;
+		x[i] = aux;
 	}
 	fscanf(fp,"\n");
 	for (i=0;i<n;i++){
 		fscanf(fp,"%c", &aux);
   	//vai colocando os valores no vetor
-		vetory[i] = aux;
+		y[i] = aux;
 	}
 		fscanf(fp,"\n");
-			fscanf(fp,"%d %d %d %d %d %d\n", &Cc, &Cr, &Cd, &Ci, &Ct, &Ck);	
+		
+		//Le do arquivo os custos de cada operação
+		fscanf(fp,"%d %d %d %d %d %d\n", &Cc, &Cr, &Cd, &Ci, &Ct, &Ck);	
 	
-	// Testando se a leitura está correta
-	 for (i=0;i<m;i++){
-			fprintf(saida,"%c",vetorx[i]);
-			printf("%c",vetorx[i]);
-	}
-	printf("\n");
-	fprintf(saida,"\n");
-	for (i=0;i<n;i++){
-			fprintf(saida,"%c",vetory[i]);
-			printf("%c",vetory[i]);
-	}
-	printf("\n");
-	printf("%d %d %d %d %d %d", Cc, Cr, Cd, Ci, Ct, Ck);
-	printf("\n");
+	//Começa a bomba!!!
+	edit_distances(x,y,m,n,Cc,Cr,Cd,Ci,Ct,Ck,T,op, &valor_i_kill);
+
+	seq_op(m, n,y,op,m,valor_i_kill);
 	
-	fprintf(saida,"\n");
-	fprintf(saida,"%d %d %d %d %d %d", Cc, Cr, Cd, Ci, Ct, Ck);
+	
+			for(i=0;i<m+1;i++){
+					for (j=0;j<n+1;j++){	
+							printf("%2c ", op[i][j]);
+						}
+						printf("\n");
+					}
+						printf("\n\n\n");
+			for(i=0;i<m;i++){
+					for (j=0;j<n;j++){	
+							printf("%2d ", T[i][j]);
+						}
+						printf("\n");
+					}
+	
+	//libera espaço na memória
+	free(x);
+	free(y);
+	free(T);
+	free(op);
 
-
-	free(vetorx);
-	free(vetory);
-
-
+	//fecha os arquivos abertos
 	fclose(fp);
 	fclose(saida);
 	return 0;
 }
+
+
